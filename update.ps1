0b3ecc1c73e1f2f46ab93ddfe7c326dda387a0a9
asmir <asmir.belkic@gmail.com>
2022-06-03 21:22:55 +0200

    update

--- update.ps1
+++ update.ps1
@@ -29,11 +29,9 @@ catch [System.Exception] {
 }
 
 Write-Output "[*] Termine!"
-Write-Output "`nScript by Asmir BELKIC - GitHub : https://github.com/asmirbelkic"
-Start-Sleep -Seconds 5
-Remove-Item "$($PWD.Path)\update.ps1" -Force 
 
 $msgBody = "Mise a jour termine vous pouvez relancer ieSolw"
 [System.Windows.MessageBox]::Show($msgBody)
-Clear-Host
+Remove-Item "$($PWD.Path)\update.ps1" -Force 
+Start-Sleep -Seconds 2
 exit
